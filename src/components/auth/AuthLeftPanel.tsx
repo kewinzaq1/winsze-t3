f474c5586985d2959e9440f9c58cbf05bc18e677
Kewin <kewinszlezingier@gmail.com>
2022-11-24 08:47:25 +0100

    refactor: move left panel to own component

---
+++ src/components/auth/AuthLeftPanel.tsx
@@ -0,0 +1,17 @@
+import { TestimonialsSlider } from "./TestimonialsSilder";
+
+export function AuthLeftPanel() {
+  return (
+    <div className="hidden h-full w-1/4 flex-col justify-between rounded-md bg-violetPrimary px-4 py-10 text-white shadow-sm lg:flex">
+      <p className="text-xl font-normal">Winsze - T3</p>
+      <div className="flex flex-col">
+        <h1 className="text-4xl">Start your journey with us.</h1>
+        <p className="lg:font-2xl mt-4 font-extralight">
+          Discover the world&apos;s best community of freelancers and business
+          owners
+        </p>
+      </div>
+      <TestimonialsSlider />
+    </div>
+  );
+}
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,24 +1,14 @@
-import { TestimonialsSlider } from "./TestimonialsSilder";
 import { BsFacebook, BsGoogle } from "react-icons/bs";
 import { AuthFormGroup } from "./AuthFormGroup";
 import { AuthLabel } from "./AuthLabel";
 import { AuthInput } from "./AuthInput";
 import { AuthButton } from "./AuthButton";
+import { AuthLeftPanel } from "./AuthLeftPanel";
 
 export const SignUp = () => {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center p-4 lg:flex-row">
-      <div className="hidden h-full w-1/4 flex-col justify-between rounded-md bg-violetPrimary px-4 py-10 text-white shadow-sm lg:flex">
-        <p className="text-xl font-normal">Winsze - T3</p>
-        <div className="flex flex-col">
-          <h1 className="text-4xl">Start your journey with us.</h1>
-          <p className="lg:font-2xl mt-4 font-extralight">
-            Discover the world&apos;s best community of freelancers and business
-            owners
-          </p>
-        </div>
-        <TestimonialsSlider />
-      </div>
+      <AuthLeftPanel />
       <div className="flex h-3/4 w-full flex-col px-10 py-4 lg:w-3/4">
         <div>
           <h2 className="text-2xl font-semibold">Sign up</h2>

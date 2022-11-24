413f26beacb455565446217fb9e838a03ba83393
Kewin <kewinszlezingier@gmail.com>
2022-11-24 10:19:29 +0100

    feat: add credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,14 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import { TRPCError } from "@trpc/server";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -22,6 +26,40 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text", placeholder: "john@doe.com" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new TRPCError({
+            code: "PARSE_ERROR",
+            message: "Missing email or password",
+          });
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+        if (!user) {
+          throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials?.password,
+          user.password
+        );
+        if (!isValid) {
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Invalid password",
+          });
+        }
+        return user;
+      },
     }),
     // ...add more providers here
   ],
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,4 +1,7 @@
+import { z } from "zod";
+import bcrypt from "bcrypt";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { prisma } from "src/server/db/client";
 
 export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
@@ -7,4 +10,22 @@ export const authRouter = router({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+  register: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z.string().min(8),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const password = await bcrypt.hash(input.password, 10);
+
+      const user = await ctx.prisma.user.create({
+        data: {
+          email: input.email,
+          password: password,
+        },
+      });
+      return user;
+    }),
 });

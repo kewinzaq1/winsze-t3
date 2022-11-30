55218465bfb4ff0fa4ce96a142db1cf97bbac57a
Kewin <kewinszlezingier@gmail.com>
2022-11-30 18:53:14 +0100

    fix: make navbar fixed

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,15 +4,19 @@ import { Button } from "../common/Button";
 
 export const Navbar = () => {
   return (
-    <nav className="flex w-full items-center justify-between overflow-x-hidden rounded-b-md bg-white bg-opacity-80 p-4 text-slate-700 shadow-md backdrop-blur">
+    <nav className="absolute flex w-full items-center justify-between overflow-x-hidden rounded-b-md bg-white bg-opacity-80 p-4 text-slate-700 shadow-md backdrop-blur">
       <div className="flex-1 font-bold uppercase text-violetPrimary">
         winsze t3
       </div>
-      <div className="flex flex-1 items-center justify-end gap-2">
-        <Button variant="secondary" className="!p-2 text-sm">
-          Join us
-        </Button>
-        <Button className="!p-2 text-sm">Login</Button>
+      <div className="flex items-center justify-end gap-2 px-2">
+        <Link href="/sign-in">
+          <Button variant="secondary" className="!p-2 text-sm">
+            Join us
+          </Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button className="!p-2 text-sm">Login</Button>
+        </Link>
       </div>
     </nav>
   );

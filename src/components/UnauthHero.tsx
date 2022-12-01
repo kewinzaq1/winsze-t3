73971c394ccfebb1a9810d5a850c9e469da7fb6b
Kewin <kewinszlezingier@gmail.com>
2022-12-01 18:24:28 +0100

    fix: hero on mobile

--- src/components/UnauthHero.tsx
+++ src/components/UnauthHero.tsx
@@ -6,7 +6,7 @@ import { Button } from "./common/Button";
 
 export const UnauthHero = () => (
   <section id="hero" className="flex w-screen flex-col overflow-hidden">
-    <div className="absolute -top-[40vh] -z-30 h-screen w-full">
+    <div className="absolute  -z-30 h-[50vh] w-full">
       <Image
         src={welcomeimage}
         alt="fancy gradient background overflow-hidden"
@@ -14,9 +14,9 @@ export const UnauthHero = () => (
         className="skew-y-[-20deg]"
       />
     </div>
-    <div className="mx-auto flex h-screen w-full max-w-[1400px] items-center justify-between gap-10">
+    <div className="mx-auto flex h-screen w-full max-w-[1400px] flex-col-reverse items-center justify-center gap-10 px-10 pt-24 md:flex-row md:justify-between">
       <div className="flex flex-col">
-        <h1 className="text-7xl font-bold">
+        <h1 className="text-4xl font-bold md:text-7xl">
           <p>Winsze is more than app</p>
           <span className="block overflow-visible bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
             This is a community

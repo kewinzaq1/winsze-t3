c3560baa68c9146c90b72c0f102733b3f0526aca
Kewin <kewinszlezingier@gmail.com>
2022-12-01 10:07:10 +0100

    feat: add buton join us in hero

--- src/components/UnauthHero.tsx
+++ src/components/UnauthHero.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import welcomeimage from "src/assets/background/welcome.png";
 import moreThanTeam from "src/assets/more-than-team.svg";
+import { Button } from "./common/Button";
 
 export const UnauthHero = () => (
   <section id="hero" className="flex w-screen flex-col overflow-hidden">
@@ -13,13 +15,23 @@ export const UnauthHero = () => (
       />
     </div>
     <div className="mx-auto flex h-screen w-full max-w-[1400px] items-center justify-between gap-10">
-      <h1 className="text-7xl font-bold">
-        <p>Winsze is more than app</p>
-        <span className="block overflow-visible bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
-          This is a community
-        </span>
-        <span>to join all geeks</span>
-      </h1>
+      <div className="flex flex-col">
+        <h1 className="text-7xl font-bold">
+          <p>Winsze is more than app</p>
+          <span className="block overflow-visible bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
+            This is a community
+          </span>
+          <span>to join all geeks</span>
+        </h1>
+        <Link href="/sign-up">
+          <Button
+            className="mt-4 w-max rounded-full px-10 transition hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500"
+            variant="secondary"
+          >
+            Join us
+          </Button>
+        </Link>
+      </div>
       <Image src={moreThanTeam} alt="team" width={500} height={500} />
     </div>
   </section>

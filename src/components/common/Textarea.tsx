c3dfa7b16f11cfb12960a5325f50a45a3fe3cc49
Kewin <kewinszlezingier@gmail.com>
2022-12-09 15:34:43 +0100

    feat: change ring color

--- src/components/common/Textarea.tsx
+++ src/components/common/Textarea.tsx
@@ -20,7 +20,7 @@ export const Textarea = forwardRef(
       <textarea
         ref={ref as LegacyRef<HTMLTextAreaElement>}
         {...restProps}
-        className={`${className} min-h-3/4 h-max w-full resize-none rounded-md p-4 text-xl outline-none focus:border-transparent focus:outline-none focus:ring-2 focus:ring-violetPrimary`}
+        className={`${className} min-h-3/4 h-max w-full resize-none rounded-md p-4 text-xl outline-none focus:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-700`}
       />
     );
   }

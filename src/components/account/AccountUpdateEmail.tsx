990358caaa8f16f9b118e6a60b735c550d4eedf2
Kewin <kewinszlezingier@gmail.com>
2022-12-12 08:53:03 +0100

    perf: add notify and remove old message

--- src/components/account/AccountUpdateEmail.tsx
+++ src/components/account/AccountUpdateEmail.tsx
@@ -6,9 +6,26 @@ import { Input } from "src/components/common/Input";
 import { Label } from "src/components/common/Label";
 import { trpc } from "src/utils/trpc";
 import { z } from "zod";
+import { useNotifier } from "../notifier";
 
 export function AccountUpdateEmail() {
-  const { mutate, isLoading, error } = trpc.account.updateEmail.useMutation();
+  const { show } = useNotifier();
+  const { mutate, isLoading } = trpc.account.updateEmail.useMutation({
+    onSuccess: () => {
+      show({
+        message: "Email updated",
+        description: "Your email has been updated",
+        type: "success",
+      });
+    },
+    onError: (err) => {
+      show({
+        message: "Error updating email",
+        description: err.message,
+        type: "error",
+      });
+    },
+  });
 
   const {
     register,
@@ -37,11 +54,6 @@ export function AccountUpdateEmail() {
       className="relative mt-4 flex h-full w-full flex-col items-start"
     >
       <h2 className="text-2xl font-semibold">Update Email</h2>
-      {error && (
-        <p className="absolute top-8 m-0 p-0 text-sm text-red-500">
-          {error.message}
-        </p>
-      )}
       <FormGroup className="w-full">
         <FormGroup className="w-full">
           <Label>New Email</Label>
@@ -67,7 +79,6 @@ export function AccountUpdateEmail() {
         type="submit"
         className="mt-5 w-full text-center"
         isLoading={isLoading}
-        variant={isLoading ? "primary" : "secondary"}
       >
         Update
       </Button>

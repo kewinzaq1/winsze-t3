e7f57283b78bdf80eca4fe8890644f2207f885e0
Kewin <kewinszlezingier@gmail.com>
2022-12-13 09:36:44 +0100

    perf: add name to header

--- src/components/account/AccountHeader.tsx
+++ src/components/account/AccountHeader.tsx
@@ -7,7 +7,7 @@ export const AccountHeader = () => {
     <header className="col-span-4 mx-auto mb-4 max-w-7xl pt-24">
       <h1 className="text-4xl font-bold">Account</h1>
       <p className="text-2xl text-gray-500">
-        Welcome, {session.data?.user?.email}
+        Welcome, {session.data?.user?.name ?? session.data?.user?.email}
       </p>
     </header>
   );

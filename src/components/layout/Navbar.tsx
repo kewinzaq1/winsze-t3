2d211732654f61a00ff3783d956df075b6ee2d30
Kewin <kewinszlezingier@gmail.com>
2022-12-17 11:23:31 +0100

    chore: resolve conflicts

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import { useState } from "react";
-import { Button } from "../common/Button";
-
-export const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
-  return (
-    <nav className="flex items-center justify-between rounded-b-md bg-white bg-opacity-80 p-4 text-slate-700 shadow-md backdrop-blur">
-      <div className="flex-1 font-bold uppercase text-violetPrimary">
-        winsze t3
-      </div>
-      <div className="flex flex-1 items-center justify-end gap-2">
-        <Button variant="secondary" className="!p-2 text-sm">
-          Join us
-        </Button>
-        <Button className="!p-2 text-sm">Login</Button>
-      </div>
-    </nav>
-=======
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -125,6 +103,5 @@ export const Navbar = () => {
         )}
       </div>
     </div>
->>>>>>> origin/master
   );
 };

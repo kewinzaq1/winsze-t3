91d1219dfbbb9a2133a45237b54bc9cbb51a051b
Kewin <kewinszlezingier@gmail.com>
2022-12-20 18:46:12 +0100

    feat: add usersRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,12 +3,14 @@ import { accountRouter } from "./account";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
 import { postsRouter } from "./posts";
+import { usersRouter } from "./users";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
   account: accountRouter,
   posts: postsRouter,
+  users: usersRouter,
 });
 
 // export type definition of API

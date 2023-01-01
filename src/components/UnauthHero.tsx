486c2d3055dd0e584ce557f0fa1de623f7fb9e2c
Kewin <kewinszlezingier@gmail.com>
2023-01-01 20:39:20 +0100

    fix: wrapping text

--- src/components/UnauthHero.tsx
+++ src/components/UnauthHero.tsx
@@ -6,16 +6,16 @@ import { Button } from "./common/Button";
 
 export const UnauthHero = () => (
   <section id="hero" className="flex w-screen flex-col overflow-hidden">
-      <Image
-        src={welcomeimage}
-        alt="fancy gradient background overflow-hidden"
-        width={1920}
-        height={1280}
-        className="w-[50vw] hidden md:block md:absolute h-screen object-cover -z-30"
-        loading="eager"
-      />
+    <Image
+      src={welcomeimage}
+      alt="fancy gradient background overflow-hidden"
+      width={1920}
+      height={1280}
+      className="-z-30 hidden h-screen w-[50vw] object-cover md:absolute md:block"
+      loading="eager"
+    />
     <div className="mx-auto flex h-screen w-full flex-col-reverse items-center justify-center gap-10 px-10 pt-24 md:flex-row md:justify-between">
-      <div className="flex flex-col">
+      <div className="flex flex-col md:w-[50vw] md:pr-4">
         <h1 className="text-4xl font-bold md:text-7xl">
           <p>Winsze is more than app</p>
           <span className="block overflow-visible bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
@@ -32,8 +32,8 @@ export const UnauthHero = () => (
           </Button>
         </Link>
       </div>
-      <div className="flex items-center justify-center w-full md:w-1/2">
-      <Image src={moreThanTeam} alt="team" width={500} height={500} />
+      <div className="flex w-full items-center justify-center md:w-1/2">
+        <Image src={moreThanTeam} alt="team" width={500} height={500} />
       </div>
     </div>
   </section>

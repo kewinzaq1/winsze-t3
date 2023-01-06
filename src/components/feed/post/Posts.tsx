fd134df2caff7efd902bc41c4590e598f5aa8782
Kewin <kewinszlezingier@gmail.com>
2023-01-06 11:34:43 +0100

    style: remove unused imports

--- src/components/feed/post/Posts.tsx
+++ src/components/feed/post/Posts.tsx
@@ -1,13 +1,9 @@
-import { useEffect } from "react";
 import { trpc } from "src/utils/trpc";
 import { Post } from "./Post";
-import { PostSkeleton } from "./PostSkeleton";
 import { PostSkeletons } from "./PostSkeletons";
 
 export const Posts = () => {
   const { data: posts, isLoading } = trpc.posts.getPosts.useQuery({});
-
-  console.log("posts", posts);
 
   return (
     <div className="mx-auto mt-10 w-full max-w-xl">

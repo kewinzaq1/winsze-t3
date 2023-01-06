65b9dcfc15890c80efcf41afed2e7fc951f4c671
Kewin <kewinszlezingier@gmail.com>
2023-01-06 19:43:29 +0100

    feat: implement add/remove user from friends

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,14 +1,20 @@
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import { Post } from "src/components/feed/post/Post";
+import type { RouterOutputs } from "src/utils/trpc";
 import { trpc } from "src/utils/trpc";
 import Image from "next/image";
 import avatarPlaceholder from "src/images/avatar_placeholder.png";
 import notFoundUser from "src/assets/not_found_user.png";
 import { SingleUserSkeleton } from "src/components/users/SingleUser/SingleUserSkeleton";
+import { Button } from "src/components/common/Button";
+import { useNotifier } from "src/components/notifier";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function SingleUserPage() {
   const router = useRouter();
+  const { show } = useNotifier();
+  const queryClient = useQueryClient();
 
   const id = router.query.id as string;
   const { data: user, isLoading } = trpc.users.getUser.useQuery(
@@ -17,6 +23,76 @@ export default function SingleUserPage() {
     },
     { enabled: Boolean(id) }
   );
+
+  const { mutate: addFriend, isLoading: isAddingFriend } =
+    trpc.friends.addUserToFriends.useMutation({
+      onSuccess: () => {
+        show({
+          type: "success",
+          message: "User added to friends",
+          description: "You can now see his posts in your feed",
+        });
+      },
+      onError: (error) => {
+        show({
+          type: "error",
+          message: "Error",
+          description: error.message,
+        });
+      },
+      onMutate: (newFriend) => {
+        const QUERY = [["users", "getUser"], { input: { id }, type: "query" }];
+        queryClient.cancelQueries(QUERY);
+
+        const previousValue = queryClient.getQueryData(QUERY);
+
+        queryClient.setQueryData(QUERY, (old: unknown) => {
+          const oldData = old as RouterOutputs["users"]["getUser"];
+          return {
+            ...oldData,
+            Friend: [...(oldData?.Friend || []), newFriend],
+          };
+        });
+
+        return previousValue;
+      },
+    });
+
+  const { mutate: removeFriend, isLoading: isRemovingFriend } =
+    trpc.friends.removeUserFromFriends.useMutation({
+      onSuccess: () => {
+        show({
+          type: "success",
+          message: "User removed from friends",
+          description: "You can no longer see his posts in your feed",
+        });
+      },
+      onError: (error) => {
+        show({
+          type: "error",
+          message: "Error",
+          description: error.message,
+        });
+      },
+      onMutate: () => {
+        const QUERY = [["users", "getUser"], { input: { id }, type: "query" }];
+        queryClient.cancelQueries(QUERY);
+
+        const previousValue = queryClient.getQueryData(QUERY);
+
+        queryClient.setQueryData(QUERY, (old: unknown) => {
+          const oldData = old as RouterOutputs["users"]["getUser"];
+          return {
+            ...oldData,
+            Friend: oldData?.Friend?.filter((friend) => friend.userId !== id),
+          };
+        });
+
+        return previousValue;
+      },
+    });
+
+  const isYourFriend = user?.Friend?.some((friend) => friend.userId === id);
 
   if (!user && !isLoading) {
     return (
@@ -34,7 +110,7 @@ export default function SingleUserPage() {
   return (
     <div className="mx-auto flex max-w-xl flex-col items-center justify-center pt-24">
       <header className="flex w-full flex-col justify-between gap-2">
-        <div className="flex items-center justify-between gap-2">
+        <div className="flex justify-between gap-2">
           {user?.image ? (
             <Image
               src={user?.image}
@@ -54,7 +130,7 @@ export default function SingleUserPage() {
               className="h-16 w-16 rounded-full object-cover"
             />
           )}
-          <div className="flex flex-col justify-end">
+          <div className="flex flex-col justify-start">
             <h1 className="text-4xl">{user?.name || user?.email}</h1>
             <div className="flex items-center justify-end gap-1">
               <p className="w-max rounded-full bg-blue-100 px-2 py-1">
@@ -64,6 +140,22 @@ export default function SingleUserPage() {
                 {user?.role || "user"}
               </p>
             </div>
+            <Button
+              className="ml-auto mt-4 w-max justify-self-end"
+              disabled={isYourFriend ? isRemovingFriend : isAddingFriend}
+              isLoading={isYourFriend ? isRemovingFriend : isAddingFriend}
+              onClick={() => {
+                if (!id) return;
+
+                if (isYourFriend) {
+                  removeFriend({ userId: id as string });
+                  return;
+                }
+                addFriend({ userId: id as string });
+              }}
+            >
+              {isYourFriend ? "Remove from friends" : "Add to friends"}
+            </Button>
           </div>
         </div>
       </header>

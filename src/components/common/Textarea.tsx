7ab6f24f4d2a76e3559a00858f05f9eca26e10da
Kewin <kewinszlezingier@gmail.com>
2023-01-06 15:57:37 +0100

    chore: add auto overflow in textarea

--- src/components/common/Textarea.tsx
+++ src/components/common/Textarea.tsx
@@ -20,7 +20,7 @@ export const Textarea = forwardRef(
       <textarea
         ref={ref as LegacyRef<HTMLTextAreaElement>}
         {...restProps}
-        className={`${className} min-h-3/4 scrollbar-rounded-md h-max w-full resize-none rounded-md p-4 outline-none focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-300`}
+        className={`${className} min-h-3/4 scrollbar-rounded-md h-max w-full resize-none overflow-y-auto rounded-md p-4 outline-none focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-300`}
       />
     );
   }

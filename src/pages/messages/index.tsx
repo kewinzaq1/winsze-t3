39db834614996545e679abdee4d7b0157cdb5f9e
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-01-26 18:17:19 +0100

    feat: add messages page

---
+++ src/pages/messages/index.tsx
@@ -0,0 +1,11 @@
+import { MessagesLayout } from "src/components/messages/layout";
+
+export default function Messages() {
+  return (
+    <MessagesLayout>
+      <div>
+        <p>Choose a user from the sidebar to view their messages.</p>
+      </div>
+    </MessagesLayout>
+  );
+}

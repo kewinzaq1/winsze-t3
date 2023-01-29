149df7d060811b0ff2dd645ffc0b6a8bc554e23a
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-01-29 09:37:52 +0100

    feat: add follower skeleton message

---
+++ src/components/messages/FollowerSkeleton.tsx
@@ -0,0 +1,11 @@
+export function FollowerSkeleton() {
+  return (
+    <div className="flex items-center gap-2 rounded-md bg-slate-50 p-4 shadow-sm">
+      <div className="h-12 w-12 rounded-full bg-slate-200"></div>
+      <div className="flex flex-col gap-1 truncate">
+        <div className="h-4 w-24 bg-slate-200"></div>
+        <div className="h-4 w-32 bg-slate-200"></div>
+      </div>
+    </div>
+  );
+}

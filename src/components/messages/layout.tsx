a8c3fc0bd8b3832958d74ebc28af641bca67dc76
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-02-01 18:32:11 +0100

    refactor: messages layout

--- src/components/messages/layout.tsx
+++ src/components/messages/layout.tsx
@@ -1,12 +1,26 @@
+import { useSession } from "next-auth/react";
 import type { ReactNode } from "react";
 import { trpc } from "src/utils/trpc";
 import { useInitWebsocket } from "src/utils/useInitWebSocket";
-import { Follower } from "./Follower";
-import { FollowerSkeleton } from "./FollowerSkeleton";
+import { ConversationCard } from "./ConverationCard";
+import { ConversationCardSkeleton } from "./ConversationCardSkeleton";
 
 export const MessagesLayout = ({ children }: { children: ReactNode }) => {
   useInitWebsocket();
-  const { data: users, isLoading } = trpc.follow.getFollowers.useQuery({});
+  const { data: session } = useSession();
+
+  const { data, isLoading } = trpc.chat.getAllConversations.useQuery();
+
+  const users = data?.map((conversation) => {
+    const otherUser = conversation.participants.find(
+      (user) => user.id !== session?.user?.id
+    );
+
+    return {
+      conversation,
+      user: otherUser,
+    };
+  });
 
   const SKELETONS_ARR = new Array(8).fill(0);
 
@@ -16,11 +30,18 @@ export const MessagesLayout = ({ children }: { children: ReactNode }) => {
         <h1 className="text-center text-2xl font-bold">Messages</h1>
         <hr className="my-4" />
         <ul className="flex h-full flex-col gap-2 overflow-y-auto px-4 pb-20">
-          {users?.map((user) => (
-            <Follower key={user.user.id} user={user} />
-          ))}
+          {users?.map(
+            (user) =>
+              user.user && (
+                <ConversationCard
+                  key={user.user.id}
+                  user={user.user}
+                  conversationId={user.conversation.id}
+                />
+              )
+          )}
           {isLoading &&
-            SKELETONS_ARR.map((_, i) => <FollowerSkeleton key={i} />)}
+            SKELETONS_ARR.map((_, i) => <ConversationCardSkeleton key={i} />)}
         </ul>
       </aside>
       {children}

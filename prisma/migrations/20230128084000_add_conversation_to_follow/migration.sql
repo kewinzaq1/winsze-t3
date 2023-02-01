b0c836ce2aabff88bb5f9333e31ec0bce03f6b1d
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-02-01 18:30:39 +0100

    chore: minor changes

--- prisma/migrations/20230128084000_add_conversation_to_follow/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Follow` ADD COLUMN `conversationId` VARCHAR(191) NULL;
-
--- CreateIndex
-CREATE INDEX `Follow_conversationId_idx` ON `Follow`(`conversationId`);
--- prisma/migrations/20230129081350_fix_conversation/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Conversation_userId_key` ON `Conversation`;
-
--- DropIndex
-DROP INDEX `Follow_followerId_idx` ON `Follow`;
